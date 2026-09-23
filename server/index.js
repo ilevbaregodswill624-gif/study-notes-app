9c420be576108c6917601430ab4990b636cc7476
ilevbaregodswill624-gif <ilevbaregodswill624@gmail.com>
2026-09-23 12:28:54 +0100

    MongoDB connected successfully

--- server/index.js
+++ server/index.js
@@ -1,8 +1,15 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
 const express = require("express");
 const cors = require("cors");
 
 const app = express();
 const PORT = 5000;
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.error('MongoDB connection error:', err));
 
 app.use(cors());
 app.use(express.json());
